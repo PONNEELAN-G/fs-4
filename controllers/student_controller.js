c5eb05e921343c8d283adf62e4f39b2ff21f843f
Ponneelan G <ponneelan98@gmail.com>
2025-05-26 11:46:12 +0530

    final crud

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -1,19 +1,63 @@
-export const readStudents=(req,res)=>{
-    res.send('Read students')
+import Student from "../models/student_model.js"
+
+export const readStudents=async(req,res)=>{
+    try {
+        const students=await Student.find()
+        res.json(students)
+    } catch (error) {
+        return res.json({message:error.message})
+    }
 }
 
-export const readStudent=(req,res)=>{
-
+export const readStudent=async(req,res)=>{
+    try {
+        const student=await Student.findById(req.params.id)
+        res.json(student)
+    } catch (error) {
+        return res.json({message:error.message})
+    }
 }
 
-export const createStudent=(req,res)=>{
-    res.send('Create student')
+export const createStudent=async(req,res)=>{
+    const newStudent=new Student({
+        id:req.body.id,
+        name:req.body.name,
+        place:req.body.place
+    })
+    try {
+        await newStudent.save()
+        res.json(newStudent)
+    } catch (error) {
+        return res.json({message:error.message})
+    }
 }
 
-export const updateStudent=(req,res)=>{
-
+export const updateStudent=async(req,res)=>{
+    try {
+        const updatedStudent=await Student.findByIdAndUpdate(
+            {
+                _id:req.params.id
+            },
+            {
+                id:req.body.id,
+                name:req.body.name,
+                place:req.body.place
+            },
+            {
+                new:true
+            }
+        )
+        res.json(updatedStudent)
+    } catch (error) {
+        return res.json({message:error.message})
+    }
 }
 
-export const deleteStudent=(req,res)=>{
-    
+export const deleteStudent=async(req,res)=>{
+    try {
+        await Student.deleteOne({_id:req.params.id})
+        res.json({message:"Deleted successfully"})
+    } catch (error) {
+        return res.json({message:error.message})
+    }
 }
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import router from './routers/student_router.js'
+import connectDB from './lib/db.js'
+import cors from 'cors'
 
 const app=express()
 const PORT=4000
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+connectDB()
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
